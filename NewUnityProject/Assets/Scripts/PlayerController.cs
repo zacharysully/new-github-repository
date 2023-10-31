7781252e578a7f6583c37fe8166597cc317f2ab8
Jeff-CAGD <zacharysully@gmail.com>
2023-10-31 11:36:11 -0700

    updated player move

--- NewUnityProject/Assets/Scripts/PlayerController.cs
+++ NewUnityProject/Assets/Scripts/PlayerController.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
+    private void Move()
+    {
+        //moves the player
+    }    
     //Code for player movement Done
 }
