ddd82f64c4b6a5bedfe4e1bbefc9987b4d272010
Jeff-CAGD <zacharysully@gmail.com>
2023-10-31 11:38:57 -0700

    Updated scene and playercontroller

--- NewUnityProject/Assets/Scripts/PlayerController.cs
+++ NewUnityProject/Assets/Scripts/PlayerController.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector3 moveDirection;
+    private int speed = 10;
+
+    public void Jump()
+    {
+        //player can jump
+    }
+
+    public void Movement()
+    {
+        //moves the player
+    }
     //Code for player movement Done
 }
